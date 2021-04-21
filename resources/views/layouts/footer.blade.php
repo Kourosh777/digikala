f4f19eb29f1ed03378c0ab54383609849669ab46
Kourosh H <kourosh.h777@gmail.com>
2021-04-21 12:55:40 +0430

    commit 2

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,240 @@
+<!-- component 7 -->
+
+<div class="component7 container-fluid">
+    <div class="component-body2">
+        <div class="row my-border-top">
+            <div class="col-3">
+                <div class="footer-links">
+                    <div class="footer-link-header mb-2">راهنمای خرید از دیجی‌کالا</div>
+                    <ul class="list-unstyled link-unstyled">
+                        <li><a href="">نحوه ثبت سفارش</a></li>
+                        <li><a href="">رویه ارسال سفارش</a></li>
+                        <li><a href="">شیوه های پرداخت</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="footer-links">
+                    <div class="footer-link-header mb-2">خدمات مشتریان</div>
+                    <ul class="list-unstyled link-unstyled">
+                        <li><a href="">رویه‌های بازگرداندن کالا</a></li>
+                        <li><a href="">شرایط استفاده</a></li>
+                        <li><a href="">حریم خصوصی</a></li>
+                        <li><a href="">گزارش باگ</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="footer-links">
+                    <div class="footer-link-header mb-2">با دیجی‌کالا</div>
+                    <ul class="list-unstyled link-unstyled">
+                        <li><a href=""> اتاق خبر دیجی‌کالا</a></li>
+                        <li><a href="">فروش در دیجی‌کالا</a></li>
+                        <li><a href="">فرصت‌های شغلی</a></li>
+                        <li><a href="">تماس با دیجی‌کالا</a></li>
+                        <li><a href="">درباره دیجی‌کالا</a></li>
+                        <li><a href="">راهنمای هویت بصری</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-3 ">
+                <form action="">
+                    <fieldset>
+                        <legend class="fieldset pt-4">
+                            <div>از تخفیف‌ها و جدیدترین‌های دیجی‌کالا باخبر شوید:</div>
+                        </legend>
+                        <div class="input-group mb-4 mt-3 footer-input">
+                            <input type="text" class="form-control" placeholder="آدرس ایمیل خود را وارد کنید"
+                                   aria-label="آدرس ایمیل خود را وارد کنید" aria-describedby="basic-addon2">
+                            <div class="input-group-append">
+                                <button class="btn btn-primary footer-btn" type="button">ارسال</button>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+                <div class="footer-social">
+                    <div>
+                        دیجی‌کالا را در شبکه‌های اجتماعی دنبال کنید:
+                    </div>
+                    <div class="social-icons mt-4">
+                        <a href="" class="ml-3">
+                            <i class="fab fa-linkedin"></i>
+                        </a>
+                        <a href="" class="ml-3">
+                            <i class="fab fa-youtube"></i>
+                        </a>
+                        <a href="" class="ml-3">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                        <a href="" class="ml-3">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div class="footer-address pt-4">
+                <ul class="list-unstyled footer-contact d-flex justify-content-center">
+                    <li>
+                        هفت روز هفته ، ۲۴ ساعت شبانه‌روز پاسخگوی شما هستیم
+                    </li>
+                    <li class="pr-4">
+                        شماره تماس :
+                        ۶۱۹۳۰۰۰۰ - ۰۲۱
+                    </li>
+                    <li class="pr-4">
+                        آدرس ایمیل :
+                        info@digikala.com
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-address-image d-flex justify-content-center">
+                <a href="">
+                    <img src="./style/images/2.png" class="img-fluid" alt="">
+                </a>
+                <a href="">
+                    <img src="./style/images/1.svg" width="150" class="img-fluid" alt="">
+                </a>
+                <a href="">
+                    <img src="./style/images/2.svg" class="img-fluid" alt="">
+                </a>
+                <a href="">
+                    <img src="./style/images/3.png" class="img-fluid" alt="">
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- component 8 -->
+
+<div class="component8 container-fluid">
+    <div class="component-body3">
+        <div class="row">
+            <div class="col-8">
+                <div class="end-footer-content">
+                    <h1>فروشگاه اینترنتی دیجی‌کالا، بررسی، انتخاب و خرید آنلاین</h1>
+                    <p>
+                        دیجی‌کالا به عنوان یکی از قدیمی‌ترین فروشگاه های اینترنتی با بیش از یک دهه تجربه، با پایبندی
+                        به سه اصل، پرداخت در محل، 7 روز ضمانت بازگشت کالا و تضمین اصل‌بودن کالا موفق شده تا همگام با
+                        فروشگاه‌های معتبر جهان، به بزرگ‌ترین فروشگاه اینترنتی ایران تبدیل شود. به محض ورود به سایت
+                        دیجی‌کالا با دنیایی از کالا رو به رو می‌شوید! هر آنچه که نیاز دارید و به ذهن شما خطور می‌کند
+                        در اینجا پیدا خواهید کرد.
+                    </p>
+                    <div class="link-unstyled">
+                        <a href="">اسباب بازی</a>
+                        <a href="">عطر و ادوکلن</a>
+                        <a href="">تلویزیون</a>
+                        <a href="">رنگ مو و ابرو</a>
+                        <a href="">مانتو</a>
+                        <a href="">لوازم جانبی ماسک تنفسی</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <aside>
+                    <ul class="list-unstyled d-flex footer-safety-partner">
+                        <li class="d-flex">
+                            <img class="img-fluid img8" width="90" src="./style/images/4.png" alt="">
+                        </li>
+                        <li>
+                            <img class="img-fluid" src="./style/images/5.png" alt="">
+                        </li>
+                        <li>
+                            <img class="img-fluid" src="./style/images/6.png" alt="">
+                        </li>
+                    </ul>
+                </aside>
+            </div>
+        </div>
+        <nav class="footer-partners">
+            <ul class="list-unstyled d-flex justify-content-between">
+                <li>
+                    <a href="">
+                        <img src="./style/images/3.svg" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img src="./style/images/4.svg" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img src="./style/images/5.svg" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img src="./style/images/6.svg" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img src="./style/images/7.svg" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img src="./style/images/8.svg" alt="">
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <div class="footer-copyright">
+            <div class="copyright-text">
+                استفاده از مطالب فروشگاه اینترنتی دیجی‌کالا فقط برای مقاصد غیرتجاری و با ذکر منبع
+                بلامانع است. کلیه حقوق این سایت متعلق به شرکت نوآوران فن آوازه (فروشگاه آنلاین دیجی‌کالا) می‌باشد.
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+<script src="./style/js/jquery-3.5.1.min.js"></script>
+<script src="./style/js/popper.js"></script>
+<script src="./style/js/bootstrap.js"></script>
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+
+<!-- Initialize Swiper -->
+<script>
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 5,
+        slidesPerGroup: 5,
+        spaceBetween: 30,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+    var swiper2 = new Swiper('.swiper-container2', {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        spaceBetween: 20,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+</script>
+
+</body>
+
+</html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style/css/bootstrap-rtl.css">
+    <link href="./style/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+    <link href="./style/css/swiperjs.css" rel="stylesheet">
+    <link rel="stylesheet" href="style/css/style.css">
+    <title>Document</title>
+    <!-- Demo styles -->
+
+</head>
+
+<body>
+
+<!-- component 10 -->
+<header class="fixed-top">
+    <div class="component10">
+        <div class="component-body10">
+            <div class="row">
+                <div class="col">
+                    <div class="row">
+                        <div class="col-7">
+                            <div class="header-right d-flex align-items-center">
+                                <div class="logo">
+                                    <a href="">
+                                        <img src="./style/images/logo.svg" width="110" alt="">
+                                    </a>
+                                </div>
+
+                                <div class="input-group header-search">
+                                    <div class="input-group-prepend">
+                                        <span
+                                            class="input-group-text border-left-0 search-icon header-search-border"
+                                            id="basic-addon1"><i class="fa fa-search" aria-hidden="true"></i></span>
+                                    </div>
+                                    <input type="text"
+                                           class="form-control input-group-text border-right-0 text-right search-input header-search-border"
+                                           placeholder="جستجو در دیجی کالا ..." aria-label="Username"
+                                           aria-describedby="basic-addon1">
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="col-5 align-self-center">
+                            <div class="header-left d-flex justify-content-end align-items-center">
+                                <div class=" align-self-center">
+                                    <a href="" class="header-btn"><i class="fa fa-user ml-3"
+                                                                     aria-hidden="true"></i>ورود به حساب کاربری</a>
+                                </div>
+                                <div class="header-icon-shop mr-5">
+                                    <a href="" class="header-icon-shop-color">
+                                        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <nav>
+                        <div class="nav-container row justify-content-between">
+                            <div class="nav-menu col-8">
+                                <ul class="nav-list list-unstyled d-flex mb-0">
+                                    <li class="nav-item  dropdown">
+                                        <a href="">
+                                            <div class="nav-item-name">
+                                                <i class="fa fa-bars" aria-hidden="true"></i>
+                                                دسته بندی کالاها
+                                            </div>
+                                        </a>
+                                        <div class="dropdown-details">
+                                            <div class="dropdown-categories d-flex flex-column">
+                                                <a href=""><i class="fa fa-laptop ml-2" aria-hidden="true"></i>کالای
+                                                    دیجیتال </a>
+                                                <a href=""><i class="fa fa-laptop ml-2" aria-hidden="true"></i>کالای
+                                                    دیجیتال </a>
+                                                <a href=""><i class="fa fa-laptop ml-2" aria-hidden="true"></i>کالای
+                                                    دیجیتال </a>
+                                                <a href=""><i class="fa fa-laptop ml-2" aria-hidden="true"></i>کالای
+                                                    دیجیتال </a>
+                                                <a href=""><i class="fa fa-laptop ml-2" aria-hidden="true"></i>کالای
+                                                    دیجیتال </a>
+                                            </div>
+                                            <div class="dropdown-categories-content d-flex flex-column">
+                                                <div class="dropdown-content-header">
+                                                    <a href="">
+
+                                                        همه دسته‌بندی‌های کالای دیجیتال
+                                                        <i class="fa fa-angle-left mr-2" aria-hidden="true"></i>
+                                                    </a>
+                                                </div>
+                                                <ul class="list-unstyled dropdown-content pt-0 d-flex flex-column flex-wrap">
+                                                    <li><a href="">
+                                                            کیف و کاور گوشی
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            کیف و کاور گوشی
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            کیف و کاور گوشی
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            کیف و کاور گوشی
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            کیف و کاور گوشی
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            کیف و کاور گوشی
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            کیف و کاور گوشی
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            کیف و کاور گوشی
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            کیف و کاور گوشی
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            کیف و کاور گوشی
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            کیف و کاور گوشی
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            کیف و کاور گوشی
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            کیف و کاور گوشی
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            کیف و کاور گوشی
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            کیف و کاور گوشی
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            کیف و کاور گوشی
+                                                        </a></li>
+
+
+                                                </ul>
+                                            </div>
+                                            <div class="dropdown-categories-content d-none">
+                                                <div class="dropdown-content-header">
+                                                    <a href="">
+
+                                                        همه دسته‌بندی‌های کالای دیجیتال
+                                                        <i class="fa fa-angle-left mr-2" aria-hidden="true"></i>
+                                                    </a>
+                                                </div>
+                                                <ul class="list-unstyled dropdown-content pt-0 d-flex flex-column flex-wrap">
+                                                    <li><a href="">
+                                                            گوشی موبایل
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            گوشی موبایل
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            گوشی موبایل
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            گوشی موبایل
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            گوشی موبایل
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            گوشی موبایل
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            گوشی موبایل
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            گوشی موبایل
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            گوشی موبایل
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            گوشی موبایل
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            گوشی موبایل
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            گوشی موبایل
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            گوشی موبایل
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            گوشی موبایل
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            گوشی موبایل
+                                                        </a></li>
+                                                    <li><a href="">
+                                                            گوشی موبایل
+                                                        </a></li>
+
+
+                                                </ul>
+                                            </div>
+                                        </div>
+
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="">
+                                            <div class="nav-item-name">
+                                                <i class="fa fa-tag" aria-hidden="true"></i>
+                                                دسته بندی کالاها
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="">
+                                            <div class="nav-item-name">
+                                                <i class="fa fa-tag" aria-hidden="true"></i>
+                                                دسته بندی کالاها
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="">
+                                            <div class="nav-item-name">
+                                                <i class="fa fa-tag" aria-hidden="true"></i>
+                                                دسته بندی کالاها
+                                            </div>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="nav-left">
+
+                                <a href="">
+                                    <div class="nav-item-name">
+
+                                        لطفا شهر و استان خود را انتخاب کنید
+                                        <i class="fa fa-map-marker-alt mr-2" aria-hidden="true"></i>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
