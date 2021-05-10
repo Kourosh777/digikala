c00e447521df35fa67c7e2ab4f7b4157476376b9
Kourosh H <kourosh.h777@gmail.com>
2021-05-10 15:26:52 +0430

    views changes

--- resources/views/responsive-product.blade.php
+++ resources/views/responsive-product.blade.php
@@ -20,148 +20,252 @@
 </head>
 
 <body>
-    <div class="main-container">
-        <nav class="breadcrumb-container">
-            <ol class="my-breadcrumb">
-                <li class="breadcrumb-item">
-                    <a href="">
-                        <span>دیجی کالا</span>
-                    </a>
-                </li>
-                <li class="breadcrumb-item">
-                    <i class="fa fa-angle-left"></i>
-                    <a href="">
-                        <span>کالای دیجیتال</span>
-                    </a>
-                </li>
-                <li class="breadcrumb-item">
-                    <i class="fa fa-angle-left"></i>
-                    <a href="">
-                        <span>موبایل</span>
-                    </a>
-                </li>
-                <li class="breadcrumb-item">
-                    <i class="fa fa-angle-left"></i>
-                    <a href="">
-                        <span>گوشی موبایل</span>
-                    </a>
-                </li>
-            </ol>
-        </nav>
-        <article>
-            <section class="product-component">
-                <h1 class="product-title">
+<div class="main-container">
+    <nav class="breadcrumb-container">
+        <ol class="my-breadcrumb">
+            <li class="breadcrumb-item">
+                <a href="">
+                    <span>دیجی کالا</span>
+                </a>
+            </li>
+            <li class="breadcrumb-item">
+                <i class="fa fa-angle-left"></i>
+                <a href="">
+                    <span>کالای دیجیتال</span>
+                </a>
+            </li>
+            <li class="breadcrumb-item">
+                <i class="fa fa-angle-left"></i>
+                <a href="">
+                    <span>موبایل</span>
+                </a>
+            </li>
+            <li class="breadcrumb-item">
+                <i class="fa fa-angle-left"></i>
+                <a href="">
+                    <span>گوشی موبایل</span>
+                </a>
+            </li>
+        </ol>
+    </nav>
+    <article>
+        <section class="product-component">
+            <h1 class="product-title">
 
-                    گوشی موبایل سامسونگ مدل Galaxy A12 SM-A125F/DS دو سیم کارت ظرفیت 64 گیگابایت
+                گوشی موبایل سامسونگ مدل Galaxy A12 SM-A125F/DS دو سیم کارت ظرفیت 64 گیگابایت
 
-                    <div class="product-type">
-                        Samsung Galaxy A12 SM-A125F/DS Dual SIM 64GB Mobile Phone
-                    </div>
-                </h1>
-                <div class="product-options">
-                    <ul class="product-actions">
-                        <li>
-                            <button class="far fa-heart btn-option" data-toggle="tooltip" data-placement="left"
+                <div class="product-type">
+                    Samsung Galaxy A12 SM-A125F/DS Dual SIM 64GB Mobile Phone
+                </div>
+            </h1>
+            <div class="product-options">
+                <ul class="product-actions">
+                    <li>
+                        <button class="far fa-heart btn-option" data-toggle="tooltip" data-placement="left"
                                 title="Tooltip on left"></button>
-                        </li>
-                        <li>
-                            <button class="fas fa-share-alt btn-option"></button>
-                        </li>
-                        <li>
-                            <button class="fas fa-chart-line btn-option"></button>
-                        </li>
-                        <li>
-                            <button class="far fa-bell btn-option"></button>
-                        </li>
-                    </ul>
-                    <div class="product-rating">
-                        <i class="fas fa-star rating-star"></i>
+                    </li>
+                    <li>
+                        <button class="fas fa-share-alt btn-option"></button>
+                    </li>
+                    <li>
+                        <button class="fas fa-chart-line btn-option"></button>
+                    </li>
+                    <li>
+                        <button class="far fa-bell btn-option"></button>
+                    </li>
+                </ul>
+                <div class="product-rating">
+                    <i class="fas fa-star rating-star"></i>
 
-                        ۴.۴
+                    ۴.۴
 
-                        <span class="product-engagement-num">
+                    <span class="product-engagement-num">
 
                             (۱۸۸۹)
 
                         </span>
-                    </div>
                 </div>
-                <!-- swiper slider -->
-                <div class=" swiper-product">
-                    <div class="swiper-container">
-                        <div>
-                            <div class="product-img">
-                                <img src="./style/images/21.jpg" alt="">
-                            </div>
+            </div>
+            <!-- swiper slider -->
+            <div class=" swiper-product">
+                <div class="swiper-container">
+                    <div>
+                        <div class="product-img">
+                            <img src="./style/images/21.jpg" alt="">
                         </div>
                     </div>
                 </div>
-                <div class="product-jumps">
-                    <div class="product-jumps-item">
+            </div>
+            <div class="product-jumps">
+                <div class="product-jumps-item">
 
-                        برند
+                    برند
 
-                        <a class="product-jumps-text" href=""> سامسونگ</a>
-                    </div>
-                    <div class="product-jumps-item">
-
-
-
-                        <a class="product-jumps-text" href=""> گوشی موبایل سامسونگ</a>
-                    </div>
+                    <a class="product-jumps-text" href=""> سامسونگ</a>
                 </div>
-
-                <div class="product-jumps">
-                    <div class="product-jumps-item">
+                <div class="product-jumps-item">
 
 
-                        دسته‌بندی
 
-
-                        <a class="product-jumps-text" href=""> گوشی موبایل</a>
-                    </div>
+                    <a class="product-jumps-text" href=""> گوشی موبایل سامسونگ</a>
                 </div>
-            </section>
-            <section class="section-margin product-color">
-                <a href="" class="product-color-row">
+            </div>
+
+            <div class="product-jumps">
+                <div class="product-jumps-item">
+
+
+                    دسته‌بندی
+
+
+                    <a class="product-jumps-text" href=""> گوشی موبایل</a>
+                </div>
+            </div>
+        </section>
+        <section class="section-margin product-color">
+            <a href="" class="product-color-row">
 
                         <span class="product-tab-name">
                             رنگ:
                             <span class="circle-color"></span>
                             <span class="product-color">سفید</span>
                         </span>
-                        <span class="product-tab-value">
+                <span class="product-tab-value">
 
                     (۳ رنگ)
                 <i class="fas fa-angle-down"></i>
                         </span>
 
-                </a>
-            </section>
-            <section class="section-margin">
-                <div class="digiplus-box">
-                    <div class="digiplus-row">
-                        <img class="digiplus-logo" src="./style/images/10.svg" alt="">
-                    </div>
-                    <div class="digiplus-row">
-                        <span class="digiplus-box-title">خدمات ویژه کاربران دیجی‌پلاس </span>
-                    </div>
-                    <div class="digiplus-row">
+            </a>
+        </section>
+        <section class="section-margin">
+            <div class="digiplus-box">
+                <div class="digiplus-row">
+                    <img class="digiplus-logo" src="./style/images/10.svg" alt="">
+                </div>
+                <div class="digiplus-row">
+                    <span class="digiplus-box-title">خدمات ویژه کاربران دیجی‌پلاس </span>
+                </div>
+                <div class="digiplus-row">
 
-                        <span class="digiplus-box-description"><i class="fas fa-truck"></i>ارسال رایگان</span>
+                    <span class="digiplus-box-description"><i class="fas fa-truck"></i>ارسال رایگان</span>
+                </div>
+                <div class="digiplus-row">
+                    <span class="digiplus-box-description"><i class="fas fa-truck"></i>۳۰ روز بازگشت کالا</span>
+                </div>
+                <div class="digiplus-row digiplus-footer">
+                    <span class="digiplus-box-description"><i class="fas fa-truck"></i>امکان ارسال فوری</span>
+                    <a class="digiplus-box-link" href="">شما هم عضو شوید</a>
+                </div>
+            </div>
+        </section>
+        <section class="waning-text section-margin">
+            <div class="warning-text-container ">
+                <div class="fas fa-exclamation-circle warning-icon"></div>
+                <span>
+                        هشدار سامانه همتا: حتما در زمان تحویل دستگاه، به کمک کد فعال‌سازی چاپ شده روی جعبه یا کارت گارانتی، دستگاه را از طریق #7777*، برای سیم‌کارت خود فعال‌سازی کنید. آموزش تصویری در آدرس اینترنتی hmti.ir/05
+                    </span>
+            </div>
+        </section>
+        <a href="" class="feedback-link">بازخورد درباره این کالا</a>
+        <section class="product-data section-margin">
+            <div class="product-data-container">
+                <div class="product-data-row">
+                    <i class="fa fa-plus-circle digi-red-icon"></i>
+                    <div class="product-seller-info">
+                        <div class="product-seller-wrapper">فروشنده:
+                            <span class="product-seller-name">
+                                    دیجی‌کالا
+                                </span>
+                        </div>
+                        <div class="product-seller-rate-wrapper">
+                            عملکرد:
+                            <span class="product-seller-rate-of-five">
+                                    ۵ از ۵
+                                </span>
+                            <span class="product-satisfaction-wrapper">
+                                    <span class="u-divider"></span>
+                                    <span class="product-satisfaction-rate">
+                                        ۸۹.۶٪ رضایت از کالا
+                                    </span>
+                                </span>
+                        </div>
                     </div>
-                    <div class="digiplus-row">
-                        <span class="digiplus-box-description"><i class="fas fa-truck"></i>۳۰ روز بازگشت کالا</span>
-                    </div>
-                    <div class="digiplus-row digiplus-footer">
-                        <span class="digiplus-box-description"><i class="fas fa-truck"></i>امکان ارسال فوری</span>
-                        <a class="digiplus-box-link" href="">شما هم عضو شوید</a>
+                    <i class="fas fa-angle-left icon-extra"></i>
+                </div>
+                <div class="product-data-row">
+                    <i class="far fa-check-circle icon-check-circle"></i>
+                    <div class="product-guarantee">
+                        <div class="guarantee-text">
+                            گارانتی ۱۸ ماهه دیجی کالا
+                        </div>
                     </div>
                 </div>
-            </section>
-        </article>
+                <div class="product-data-row product-column-row">
+                    <div class="product-ship">
+                        <i class="fa fa-check-square icon-check-square-blue"></i>
+                        موجود در انبار دیجی‌کالا
+                        <i class="fas fa-angle-left icon-extra-2"></i>
+                    </div>
+                    <ul class="product-ship-sender">
+                        <li>
+                                <span class="product-sender-name">
+                                    <i class="fa fa-truck icon-red-truck"></i>
+                                    ارسال دیجی‌کالا
+                                </span>
+                        </li>
+                    </ul>
+                </div>
+                <div class="product-data-row product-price-wrapper">
+                    <div class="product-price-top">
+                        <div class="product-previous-price">
+                            ۳,۵۴۹,۰۰۰
+                        </div>
+                        <div class="product-discount-percent">
+                            ۳٪
+                        </div>
+                    </div>
+                    <div class="product-price-bottom">
+                        <div class="product-price-new">
+                            <span class="price-value">۳,۴۲۹,۰۰۰</span>
+                            تومان
+                        </div>
+                    </div>
+                    <div class="product-price-notification">
+                        <i class="fa fa-check-circle icon-check-notification"></i>
+                        بهترین قیمت ۳۰ روز گذشته
+                    </div>
+                </div>
+                <div class="card-view-counts">
+                    <i class="fa fa-eye icon-eye"></i>
+                    <span>۱۰۰+</span>
+                    نفر درحال بازدید این کالا می‌باشند.
+                </div>
+                <div class="product-data-row product-digiclub">
+                    <img src="./style/images/7.png" alt="">
+                    <div>
+                        <span>۱۵۰</span>
 
-    </div>
+                        امتیاز دیجی‌کلاب با خرید این کالا
+
+                    </div>
+                </div>
+                <div class="product-data-row">
+                    <div class="product-other-sellers-wrapper">
+                        <div class="product-other-sellers">
+                            <span class="my-text-bold">۲ فروشنده</span>
+                            دیگر این کالا
+                        </div>
+                        <div class="product-sellers-extra">
+                            <i class="fa fa-angle-left"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </article>
+
+</div>
 </body>
 
 </html>
