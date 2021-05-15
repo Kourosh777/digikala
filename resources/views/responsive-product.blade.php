f3aaaab65cea0e9e4420304de991f9172acb5794
Kourosh H <kourosh.h777@gmail.com>
2021-05-15 16:39:45 +0430

    add comments and footer to responsive product page

--- resources/views/responsive-product.blade.php
+++ resources/views/responsive-product.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="style/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style/css/bootstrap-rtl.css"> -->
+{{--     <link rel="stylesheet" href="style/css/bootstrap.min.css">--}}
+{{--    <link rel="stylesheet" href="style/css/bootstrap-rtl.css">--}}
 {{--    <link href="./style/css/all.css" rel="stylesheet">--}}
 {{--    <link href="./style/css/fontawesome.min.css" rel="stylesheet">--}}
 {{--    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />--}}
@@ -14,8 +14,9 @@
 {{--    <link rel="stylesheet" href="./style/css/font.css">--}}
     <link rel="stylesheet" href="{{ asset('./assets/css/app.css') }}">
 
-    <!-- <link rel="stylesheet" href="style/css/style.css"> -->
+
     <link rel="stylesheet" href="./assets/css/responsive-single-product.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
     <title>Document</title>
 </head>
 
@@ -433,15 +434,15 @@
                                 4G، 3G، 2G </span>
                     </li>
                     <li>
-                        <span>دوربین‌های پشت گوشی:  </span>
+                        <span>دوربین‌های پشت گوشی: </span>
                         <span>4 ماژول دوربین</span>
                     </li>
                     <li>
-                        <span>سیستم عامل:  </span>
+                        <span>سیستم عامل: </span>
                         <span>Android</span>
                     </li>
                     <li>
-                        <span>توضیحات سیم کارت:  </span>
+                        <span>توضیحات سیم کارت: </span>
                         <span>سایز نانو (8.8 × 12.3 میلی‌متر) </span>
                     </li>
                 </ul>
@@ -536,7 +537,332 @@
             </div>
         </div>
     </aside>
+    <div class="comments-container">
+        <div class="comments-headline">
+            مفیدترین نظرات کاربران
+            <a href="" class="comments-add">
+                افزودن نظر
+                <i class="fa fa-plus icon-plus"></i>
+            </a>
+        </div>
+        <div class="comments-digiclub">
+            <img src="./style/images/9.png" alt="">
+            <div>
+                با ثبت نظر بر روی کالاهای خریداری شده
+                <span>۵</span>
+                امتیاز در دیجی‌کلاب دریافت کنید
+            </div>
+        </div>
+        <ul class="comments-list">
+            <li>
+                <div>
+                    کاربر دیجی‌کالا
+                    <div>۲۹ دی ۱۳۹۹</div>
+                </div>
+                <p>
+                    تو بازه قیمتی خودش رقیبای سر سختی داره که به نسبت A12 ارزش خرید بیشتری دارن <br>
+                    ولی واسه کسانی که با کار کردن سامسونگ راحتترن گزینه ی مناسبیه .
+                </p>
+            </li>
+            <li>
+                <div>
+                    کاربر دیجی‌کالا
+                    <div>۲۹ دی ۱۳۹۹</div>
+                </div>
+                <p>
+                    تو بازه قیمتی خودش رقیبای سر سختی داره که به نسبت A12 ارزش خرید بیشتری دارن <br>
+                    ولی واسه کسانی که با کار کردن سامسونگ راحتترن گزینه ی مناسبیه .
+                </p>
+            </li>
+            <li>
+                <div>
+                    کاربر دیجی‌کالا
+                    <div>۲۹ دی ۱۳۹۹</div>
+                </div>
+                <p>
+                    تو بازه قیمتی خودش رقیبای سر سختی داره که به نسبت A12 ارزش خرید بیشتری دارن <br>
+                    ولی واسه کسانی که با کار کردن سامسونگ راحتترن گزینه ی مناسبیه .
+                </p>
+            </li>
+        </ul>
+        <div class="comments-actions">
+            <a href="" class="comments-show-more">
+                مشاهده همه ۲,۲۳۹ نظر کاربران
+                <i class="fa fa-angle-left"></i>
+            </a>
+        </div>
+    </div>
+    <div class="faq-container">
+        <div class="faq-headline">
+            پرسش و پاسخ
+            <button class="faq-add">
+                ارسال پرسش
+                <i class="fa fa-plus icon-plus"></i>
+            </button>
+        </div>
+        <ul class="faq-list">
+            <li class="question-wrapper">
+                <div class="faq-header">
+                    <i class="far fa-question-circle icon-question"></i>
+                    پرسش
+                    <span class="question-username-wrappers">
+                            <span class="question-username">
+                                علی محمدی
+                            </span>
+                        </span>
+                </div>
+                <p>
+                    میخواستم بپرسم اگه اثر انگشت گوشی از بین بره با چی  روشنش  کنم؟
+                </p>
+            </li>
+            <li class="question-wrapper">
+                <div class="faq-header">
+                    <i class="far fa-question-circle icon-question"></i>
+                    پرسش
+                    <span class="question-username-wrappers">
+                            <span class="question-username">
+                               رضا حسن زاده
+                            </span>
+                        </span>
+                </div>
+                <p>
+                    سلام گوشی a12پک هند است یا ویتنام؟؟
+                </p>
+            </li>
+        </ul>
+        <div class="faq-actions">
+            <a href="" class="faq-show-more">
+                مشاهده همه ۵۷۲ پرسش کاربران
+                <i class="fa fa-angle-left"></i>
+            </a>
+        </div>
+    </div>
 </div>
+
+
+
+
+<!-- component 7 -->
+
+<div class="component7">
+    <div class="component-body2">
+        <div class="row my-border-top">
+            <div class="col">
+                <div class="footer-links">
+                    <div class="footer-link-header mb-2">راهنمای خرید از دیجی‌کالا</div>
+                    <ul class="list-unstyled link-unstyled">
+                        <li><a href="">نحوه ثبت سفارش</a></li>
+                        <li><a href="">رویه ارسال سفارش</a></li>
+                        <li><a href="">شیوه های پرداخت</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col">
+                <div class="footer-links">
+                    <div class="footer-link-header mb-2">خدمات مشتریان</div>
+                    <ul class="list-unstyled link-unstyled">
+                        <li><a href="">رویه‌های بازگرداندن کالا</a></li>
+                        <li><a href="">شرایط استفاده</a></li>
+                        <li><a href="">حریم خصوصی</a></li>
+                        <li><a href="">گزارش باگ</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="d-none d-sm-flex col">
+                <div class="footer-links">
+                    <div class="footer-link-header mb-2">با دیجی‌کالا</div>
+                    <ul class="list-unstyled link-unstyled">
+                        <li><a href=""> اتاق خبر دیجی‌کالا</a></li>
+                        <li><a href="">فروش در دیجی‌کالا</a></li>
+                        <li><a href="">فرصت‌های شغلی</a></li>
+                        <li><a href="">تماس با دیجی‌کالا</a></li>
+                        <li><a href="">درباره دیجی‌کالا</a></li>
+                        <li><a href="">راهنمای هویت بصری</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-12 col-sm">
+                <form action="">
+                    <fieldset>
+                        <legend class="fieldset pt-4">
+                            <div>از تخفیف‌ها و جدیدترین‌های دیجی‌کالا باخبر شوید:</div>
+                        </legend>
+                        <div class="input-group mb-4 mt-3 footer-input">
+                            <input type="text" class="form-control" placeholder="آدرس ایمیل خود را وارد کنید"
+                                   aria-label="آدرس ایمیل خود را وارد کنید" aria-describedby="basic-addon2">
+                            <div class="input-group-append">
+                                <button class="btn btn-primary footer-btn" type="button">ارسال</button>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+                <div class="footer-social">
+                    <div>
+                        دیجی‌کالا را در شبکه‌های اجتماعی دنبال کنید:
+                    </div>
+                    <div class="social-icons mt-4">
+                        <a href="" class="ml-3">
+                            <i class="fab fa-linkedin"></i>
+                        </a>
+                        <a href="" class="ml-3">
+                            <i class="fab fa-youtube"></i>
+                        </a>
+                        <a href="" class="ml-3">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                        <a href="" class="ml-3">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div class="footer-address pt-4">
+                <ul class="list-unstyled footer-contact d-flex flex-column flex-sm-row justify-content-center">
+                    <li>
+                        هفت روز هفته ، ۲۴ ساعت شبانه‌روز پاسخگوی شما هستیم
+                    </li>
+                    <li class="pr-sm-4">
+                        شماره تماس :
+                        ۶۱۹۳۰۰۰۰ - ۰۲۱
+                    </li>
+                    <li class="pr-sm-4">
+                        آدرس ایمیل :
+                        info@digikala.com
+                    </li>
+                </ul>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="footer-address-image d-none d-sm-flex justify-content-center">
+                        <a href="">
+                            <img class="img-fluid" src="./style/images/2.png" alt="" width="150">
+                        </a>
+                        <a href="">
+                            <img class="img-fluid" src="./style/images/1.svg" alt="" width="150">
+                        </a>
+                        <a href="">
+                            <img class="img-fluid" src="./style/images/2.svg" alt="" width="150">
+                        </a>
+                        <a href="">
+                            <img class="img-fluid" src="./style/images/3.png" alt="" width="150">
+                        </a>
+                    </div>
+                    <div class="footer-address-image-responsive d-flex flex-column d-sm-none justify-content-center align-items-center">
+
+                        <div class="d-flex">
+                            <a href="">
+                                <img class="img-fluid" src="./style/images/2.png" alt="" width="150">
+                            </a>
+                            <a href="">
+                                <img class="img-fluid" src="./style/images/1.svg" alt="" width="150">
+                            </a>
+                        </div>
+                        <div class="d-flex">
+                            <a href="">
+                                <img class="img-fluid" src="./style/images/2.svg" alt="" width="150">
+                            </a>
+                            <a href="">
+                                <img class="img-fluid" src="./style/images/3.png" alt="" width="150">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- component 8 -->
+
+<div class="component8 ">
+    <div class="component-body3 component8-padding-responsive">
+        <div class="row mx-0">
+            <div class="col-sm-8 p-0">
+                <div class="end-footer-content">
+                    <h1>فروشگاه اینترنتی دیجی‌کالا، بررسی، انتخاب و خرید آنلاین</h1>
+                    <p>
+                        دیجی‌کالا به عنوان یکی از قدیمی‌ترین فروشگاه های اینترنتی با بیش از یک دهه تجربه، با پایبندی
+                        به سه اصل، پرداخت در محل، 7 روز ضمانت بازگشت کالا و تضمین اصل‌بودن کالا موفق شده تا همگام با
+                        فروشگاه‌های معتبر جهان، به بزرگ‌ترین فروشگاه اینترنتی ایران تبدیل شود. به محض ورود به سایت
+                        دیجی‌کالا با دنیایی از کالا رو به رو می‌شوید! هر آنچه که نیاز دارید و به ذهن شما خطور می‌کند
+                        در اینجا پیدا خواهید کرد.
+                    </p>
+                    <div class="link-unstyled mb-3">
+                        <a href="">اسباب بازی</a>
+                        <a href="">عطر و ادوکلن</a>
+                        <a href="">تلویزیون</a>
+                        <a href="">رنگ مو و ابرو</a>
+                        <a href="">مانتو</a>
+                        <a href="">لوازم جانبی ماسک تنفسی</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <aside>
+                    <ul class="list-unstyled d-flex footer-safety-partner">
+                        <li class="d-flex">
+                            <img class="img-fluid img8 img10" width="90" src="./style/images/4.png" alt="">
+                        </li>
+                        <li>
+                            <img class="img-fluid" width="125" src="./style/images/5.png" alt="">
+                        </li>
+                        <li>
+                            <img class="img-fluid" width="125" src="./style/images/6.png" alt="">
+                        </li>
+                    </ul>
+                </aside>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <nav class="footer-partners pt-2">
+                    <ul class="list-unstyled d-flex justify-content-between flex-wrap">
+                        <li>
+                            <a href="">
+                                <img class="img-fluid" src="./style/images/3.svg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img class="img-fluid" src="./style/images/4.svg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img class="img-fluid" src="./style/images/5.svg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img class="img-fluid" src="./style/images/6.svg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img class="img-fluid" src="./style/images/7.svg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img class="img-fluid" src="./style/images/8.svg" alt="">
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="copyright-text">
+                استفاده از مطالب فروشگاه اینترنتی دیجی‌کالا فقط برای مقاصد غیرتجاری و با ذکر منبع
+                بلامانع است. کلیه حقوق این سایت متعلق به شرکت نوآوران فن آوازه (فروشگاه آنلاین دیجی‌کالا) می‌باشد.
+
+            </div>
+        </div>
+    </div>
+</div>
+
 </body>
 
 </html>
